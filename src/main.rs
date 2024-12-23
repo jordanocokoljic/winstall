21b4dec5b282abe3787a0f45d89e3d77c5a1b6fb
Jordan Ocokoljic <jordan@nofrills.software>
2024-12-23 19:54:25 +1100

    Make error handling align with install

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,22 @@ use crate::cli::{get_options, Config};
 use std::env::args;
 use std::io::{stderr, stdout};
 
-fn main() -> winstall::Result<()> {
+fn main() {
     let config = Config::from_env();
-    let parsed = get_options(args().skip(1), config);
 
-    let (arguments, options) = match parsed {
+    let parsed = get_options(args().skip(1), config);
+    let (arguments, options) = unwrap_or_exit(parsed);
+
+    let outcome = operations::run(arguments, &options, (&mut stdout(), &mut stderr()));
+    unwrap_or_exit(outcome);
+}
+
+fn unwrap_or_exit<T>(result: Result<T, winstall::Error>) -> T {
+    match result {
         Ok(x) => x,
         Err(e) => {
-            eprintln!("{}\nTry 'winstall --help' for more information.", e);
+            eprintln!("winstall: {}\nTry 'winstall --help' for more information.", e);
             std::process::exit(1);
         }
-    };
-
-    operations::run(arguments, &options, (&mut stdout(), &mut stderr()))
+    }
 }

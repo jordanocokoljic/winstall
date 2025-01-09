6ec25fb5b6b35dba3d9c5d481d309b65bd5aacff
Jordan Ocokoljic <jordan@nofrills.software>
2025-01-09 12:07:35 +1100

    Remove unused types

--- src/foundation.rs
+++ src/foundation.rs
@@ -5,11 +5,6 @@ pub enum Operation {
     CopyFiles(Vec<std::path::PathBuf>, std::path::PathBuf),
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(PartialEq, Debug)]
-pub enum Error {}
-
 pub trait MessageRouter {
     fn out(&mut self, message: Box<dyn std::fmt::Display>);
     fn err(&mut self, message: Box<dyn std::fmt::Display>);

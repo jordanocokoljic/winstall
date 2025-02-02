7b86f485c33840af4fa484a0c32f58e81e6a6ff4
Jordan Ocokoljic <jordan@nofrills.software>
2025-02-02 22:42:05 +1100

    Add preserve timestamps support to cli

--- src/cli.rs
+++ src/cli.rs
@@ -38,6 +38,9 @@ impl Provided {
             match arg.as_str() {
                 "-v" | "--verbose" => {
                     provided.verbose = true;
+                },
+                "-p" | "--preserve-timestamps" => {
+                    provided.preserve_timestamps = true;
                 }
                 _ => (),
             }
@@ -49,8 +52,8 @@ impl Provided {
 
 #[cfg(test)]
 mod tests {
-    use std::env;
     use crate::cli::{External, Provided};
+    use std::env;
 
     #[test]
     fn external_context_from_env_gets_correct_values() {
@@ -58,8 +61,16 @@ mod tests {
         env::set_var("SIMPLE_BACKUP_SUFFIX", "my-backup-suffix");
 
         let context = External::from_env();
-        assert_eq!(context.version_control, Some("my-version-control".to_owned()));
-        assert_eq!(context.simple_backup_suffix, Some("my-backup-suffix".to_owned()));
+
+        assert_eq!(
+            context.version_control,
+            Some("my-version-control".to_owned())
+        );
+
+        assert_eq!(
+            context.simple_backup_suffix,
+            Some("my-backup-suffix".to_owned())
+        );
 
         env::remove_var("VERSION_CONTROL");
         env::remove_var("SIMPLE_BACKUP_SUFFIX");
@@ -98,6 +109,40 @@ mod tests {
                 suffix: None,
                 verbose: true,
                 preserve_timestamps: false,
+                make_all_directories: false,
+            }
+        );
+    }
+
+    #[test]
+    fn provided_parses_short_preserve_timestamps_correctly() {
+        let args = vec!["-p"].into_iter().map(str::to_owned);
+        let provided = Provided::from_arguments(args);
+
+        assert_eq!(
+            provided,
+            Provided {
+                backup: None,
+                suffix: None,
+                verbose: false,
+                preserve_timestamps: true,
+                make_all_directories: false,
+            }
+        );
+    }
+
+    #[test]
+    fn provided_parses_long_preserve_timestamps_correctly() {
+        let args = vec!["--preserve-timestamps"].into_iter().map(str::to_owned);
+        let provided = Provided::from_arguments(args);
+
+        assert_eq!(
+            provided,
+            Provided {
+                backup: None,
+                suffix: None,
+                verbose: false,
+                preserve_timestamps: true,
                 make_all_directories: false,
             }
         );
